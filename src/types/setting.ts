41af52ea1d6f25c295aa51a6a0c74d4374736983
dareczman <56695838+dareczman@users.noreply.github.com>
2025-08-11 16:28:03 +0000

    Deploying to gh-pages from @ dareczman/memory-game@114c6b3f840cef8a95ae7cd5c453ab86b77ed483 🚀

--- src/types/setting.ts
+++
@@ -1,7 +0,0 @@
-export type StepName = 'seed' | 'difficulty'
-export enum DifficultyLevel {
-  Easy = 'easy',
-  Medium = 'medium',
-  Hard = 'hard',
-}
-export type TilesDimension = { rows: number; cols: number }
--- src/utils/localStorage.ts
+++
@@ -1,5 +0,0 @@
-export const addObjectToArrayInLocalStorage = (key: string, newItem: any) => {
-  const current = JSON.parse(localStorage.getItem(key) || '[]')
-  current.push(newItem)
-  localStorage.setItem(key, JSON.stringify(current))
-}
